dfba6b4236ccc70d0f661ef94374eecc8202d706
apala <150413917+apalacodes@users.noreply.github.com>
2026-03-13 02:08:56 +0545

    Delete old src directory

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from lib.Config import Config
-
-sf = Config()
-
-print(sf.execute_query("select * from IOE.LANDING.SALES"))
